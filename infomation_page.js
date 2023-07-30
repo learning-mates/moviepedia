5d929ee006bdfe011d366d208b620bf7773d7cf2
mameruu <gkswjddbs95@naver.com>
2023-07-30 23:48:15 +0900

    home

--- infomation_page.js
+++ infomation_page.js
@@ -16,21 +16,21 @@ fetch(
     const body = document.querySelector("body"); //html body태그를 선택함
     let list = response.SJWPerform.row; //응답받은 값의 row 배열을 받음
     const ul = document.querySelector("ul"); // ul 태그 선택
-    let date = new Date(); //오늘 날짜 뽑아주기
-    let this_month = Number(date.getMonth()) + 1;
+    const img = document.querySelector(".info img");
+    const liTitle = document.querySelector(".title");
     // console.log(list[0]);
-    // body.append(list[0].AGE); 잘 나오는지 확인!!
+    // body.append(list[0].AGE);
+    //잘 나오는지 확인!!
     for (item of list) {
-      // //이 리스트에있는 각각의 아이템 값~
-      // //이미지뽑기
-      // const div = document.createElement("div");
-      // ul.append(div);
-      // const img = document.createElement("img");
-      // const img_src = item.FILE_URL_MI;
-      // img.setAttribute("src", `${img_src}`);
-      // div.append(img);
+      //이 리스트에있는 각각의 아이템 값~
+      //이미지뽑기
+      const img_src = item.FILE_URL_MI;
+      img.setAttribute("src", `${img_src}`);
 
-      // //row 아이템을 받았으묜
+      const TITLE = item.TITLE;
+      liTitle.innerText = `${TITLE}`;
+
+      //row 아이템을 받았으묜
       // const li = document.createElement("li"); // li를 선택해서 태그를 만들어주고
       // li.append(item.TITLE); // li태그에 타이틀 값을 넣어서 만들어줌
       // div.append(li); // ul에 li를 넣어서 만듬!!
@@ -39,28 +39,6 @@ fetch(
       // const day = document.createElement("li");
       // day.append(item.START_DATE);
       // div.append(day);
-
-      ul.setAttribute("style", "display: flex");
-
-      //7월만 뽑아보기 ^ㅂ^
-      let month_check = Number(item.START_DATE.slice(4, 6)) === this_month;
-      let day_check = Number(item.START_DATE.slice(6, 8)) >= date.getDate();
-      if (month_check && day_check) {
-        const card = document.createElement("div");
-        const item_img = document.createElement("img");
-        const desc_list = document.createElement("ul");
-        const item_tit = document.createElement("li");
-        const item_date = document.createElement("li");
-        item_date.innerHTML = `${item.START_DATE} - ${item.END_DATE}`;
-        item_tit.innerText = item.TITLE;
-        item_img.src = item.FILE_URL_MI;
-        item_img.setAttribute("width", "300px");
-        desc_list.append(item_tit, item_date);
-        card.append(item_img, desc_list);
-        card.classList.add("card");
-        ul.append(card);
-        ul.setAttribute("style", "display: flex");
-      }
     }
   })
   .catch((err) => console.error(err));
